c178072a02dd64a25015b72e4eff84d176267f74
01101010 <jander43@vols.utk.edu>
2017-10-03 17:21:35 -0400

    Updated with master branch

--- dokku-ml_setup.sh
+++ dokku-ml_setup.sh
@@ -5,12 +5,11 @@ wget https://raw.githubusercontent.com/dokku/dokku/v0.10.5/bootstrap.sh;
 sudo DOKKU_TAG=v0.10.5 bash bootstrap.sh
 
 # first setup CUDA
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/RagingTiger/cuda_setup/b71a8887ed704d02a00c9593c157cb5eb8d77450/cuda_setup.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/RagingTiger/cuda_setup/master/cuda_setup.sh)"
 
 # then setup nvidia-docker
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/RagingTiger/nvidia-docker_setup/743c8330e929ed773997b53798b8912760f878fe/nvidia-docker_setup.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/RagingTiger/nvidia-docker_setup/master/nvidia-docker_setup.sh)"
 
 # now create dokku app
 dokku apps:create tf-helloworld
 dokku docker-options:add tf-helloworld deploy,run "$(curl localhost:3476/docker/cli)"
-
